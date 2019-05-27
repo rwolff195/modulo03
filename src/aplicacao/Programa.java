8246a9e51eb354dd1a460eced7649749172d0e16
Rafael Wolff <wolff195@outlook.com>
2019-05-26 22:31:50 -0300

    Projeto Criado

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,47 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entidades.Triangulo;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Triangulo x, y;
+		x = new Triangulo();
+		y = new Triangulo();
+		
+		System.out.println("Entre com os lados do triangulo X: ");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		
+		System.out.println("Entre com os lados do triangulo Y: ");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		
+		
+		double areaX = x.area();
+		double areaY = y.area();
+		
+		System.out.printf("Area do Triangulo X: %.4f%n",areaX);
+		System.out.printf("Area do Triangulo Y: %.4f%n",areaY);
+		
+		if (areaX > areaY) {
+			System.out.println("Maior area: X");
+		}
+		else {
+			System.out.println("Maior area: Y");
+		}
+		
+		sc.close();
+
+	}
+
+}
